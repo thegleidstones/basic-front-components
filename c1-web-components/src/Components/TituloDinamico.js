b1f893735977b31c243ba0a63987f84236703476
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-07 00:33:41 -0300

    add props to component

--- c1-web-components/src/Components/TituloDinamico.js
+++ c1-web-components/src/Components/TituloDinamico.js
@@ -6,7 +6,7 @@ class TituloDinamico extends HTMLElement {
 
         // base do componente
         const componentRoot = document.createElement("h1");
-        componentRoot.textContent = 'Gleidson';
+        componentRoot.textContent = this.getAttribute("titulo");
 
         // estilizar o componente
         const style = document.createElement("style");
