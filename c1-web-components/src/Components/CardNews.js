1225d26d8e078dfb146fcc4b07d7d9f638edd505
Gleidson Morais <silvamorais.ti@gmail.com>
2024-11-07 01:44:56 -0300

    create a complete card component reusable with own styles.

--- c1-web-components/src/Components/CardNews.js
+++ c1-web-components/src/Components/CardNews.js
@@ -3,7 +3,89 @@ class CardNews extends HTMLElement {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card_left");
+
+        const author = document.createElement("span");
+        author.textContent = "By " + (this.getAttribute("author") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
+
+
+        cardLeft.appendChild(author);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card_right");
+
+        const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/user-default.png";
+        newsImage.alt = "Foto de Darth Vader convidando para se unir ao lado sombrio."
+        cardRight.appendChild(newsImage);
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles() {
+        const style = document.createElement("style");
+        style.textContent = 
+        `        
+            .card {
+                width: 100%;
+                display: flex;
+                flex-direction: row;
+                justify-content: space-between;
+                box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                -webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                -moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+            }
+
+            .card_left {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                padding-left: 10px;
+            }
+
+            .card_left > a {
+                margin-top: 15px;
+                font-size: 25px;
+                color: black;
+                text-decoration: none;
+                font-weight: 400;
+            }
+
+            .card_left > p {
+                color: rgb(182, 178, 178);
+            }
+
+            .card_left > span {
+                font-weight: 400;
+            }
+
+            .card_right {
+
+            }
+        `
+        return style;
+
     }
 }
 
